a11a24d0e2dee19221748548415ac8f0dbb751eb
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-10-07 22:03:51 +0100

    data import and cleaning

--- data_import.R
+++ data_import.R
@@ -39,9 +39,13 @@ ext_tracks_clean <- ext_tracks %>%
     separate(key, into = c("wind_speed", "key2")) %>% 
     spread(key2, value)
 
+
+# Some exploratory plots --------------------------------------------------
+# plotting all storms
 ggplot(data = ext_tracks_clean, aes(x = longitude, y = latitude)) +
     geom_point()
 
+# plotting hurricane Ike
 data_ike <- ext_tracks_clean %>% 
     filter(storm_name == "IKE")
 
@@ -49,9 +53,15 @@ ggplot(data = data_ike, aes(x = longitude, y = latitude)) +
     geom_point() +
     coord_map()
 
+# plotting Ike on a map
+# selecting data for all states
 usa <- map_data("usa")
+
 states <- map_data("state")
 texas <- states %>% filter(region == "texas")
+louisiana <- states %>% filter(region == "louisiana")
+
+# plotting Ike and the contour of the US
 ike_plot <- ggplot(data = states) + 
     geom_polygon(aes(x = long, y = lat, group = group), fill = "white", 
                  col = "black", alpha = 0.5) + 
@@ -61,6 +71,7 @@ ike_plot <- ggplot(data = states) +
 
 ggplotly(ike_plot, tooltip = c("label"))
 
+# plotting Ike and the contour of Texas
 texas_ike <- ggplot(data = texas) + 
     geom_polygon(aes(x = long, y = lat, group = group), fill = "white", 
                  col = "black", alpha = 0.5) + 
@@ -72,3 +83,18 @@ ggplotly(texas_ike, tooltip = c("label"))
 # based on the above visualisations use 2008-09-13 06:00:00
 data_ike_final <- data_ike %>%
     filter(date == ymd_hms("2008-09-13 06:00:00"))
+
+# go from quadrants to angles
+data_ike_quad <- data_ike_final %>%
+    gather(key = quadrant,
+           value = radius, 
+           -storm_name, -longitude, -latitude, -storm_id, -date, -wind_speed) %>% 
+    # transform quadrants in angles
+    mutate(start_angle = case_when(quadrant == "ne" ~ 0,
+                                   quadrant == "se" ~ 270,
+                                   quadrant == "sw" ~ 180,
+                                   quadrant == "nw" ~ 90),
+           end_angle = start_angle + 90) %>% 
+    # transform wind speeds from nautical miles to meters
+    mutate(wind_speed_m = 1852 * as.integer(wind_speed))
+
